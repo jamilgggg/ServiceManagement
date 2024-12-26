0b46b3e55129f1f78b88e495fa39846a7c83833a
Jamil Guardiana <jamilguardiana@gmail.com>
2024-12-26 22:45:12 +0800

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,12 +18,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'id' => 1,
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'password' => bcrypt('pass123.'),
-        // ]);
+        User::factory()->create([
+            'id' => 1,
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('pass123.'),
+        ]);
         
         Status::factory(27)->create();
         Ticket::factory(20)->create();
