bddfd844e1137bb9c1ce007f7fc59d783bb9ce51
Jamil Guardiana <jamilguardiana@gmail.com>
2025-02-25 23:40:11 +0800

    breeze login module

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::redirect('/','tick')->name('dashboard');
 
-//Route::get('/tick', [TicketController::class, 'index'])->name('tick.index');
-Route::resource('tick', TicketController::class);
-Route::get('/tick/machines/search', [TicketController::class, 'searchMachines'])->name('machines.search');
+Route::middleware(['auth','verified'])->group(function(){
+    Route::resource('tick', TicketController::class);
+    Route::get('/tick/machines/search', [TicketController::class, 'searchMachines'])->name('machines.search');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
